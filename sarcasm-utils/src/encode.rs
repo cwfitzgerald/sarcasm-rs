b9a50b40933d4484a4290da522cd4b82bbc198a2
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-08-06 07:28:09 -0400

    Remove unnecessary functions

--- sarcasm-utils/src/encode.rs
+++ sarcasm-utils/src/encode.rs
@@ -2,27 +2,17 @@ use crate::StartingCase;
 use itertools::Itertools;
 
 #[inline]
-fn char_to_lower(c: char) -> impl Iterator<Item = char> {
-    c.to_lowercase()
-}
-
-#[inline]
-fn char_to_upper(c: char) -> impl Iterator<Item = char> {
-    c.to_uppercase()
-}
-
-#[inline]
 fn encode_lower(input: &str) -> String {
-    let first = input.chars().step_by(2).flat_map(char_to_lower);
-    let second = input.chars().skip(1).step_by(2).flat_map(char_to_upper);
+    let first = input.chars().step_by(2).flat_map(|c| c.to_lowercase());
+    let second = input.chars().skip(1).step_by(2).flat_map(|c| c.to_uppercase());
 
     first.interleave(second).collect()
 }
 
 #[inline]
 fn encode_upper(input: &str) -> String {
-    let first = input.chars().step_by(2).flat_map(char_to_upper);
-    let second = input.chars().skip(1).step_by(2).flat_map(char_to_lower);
+    let first = input.chars().step_by(2).flat_map(|c| c.to_uppercase());
+    let second = input.chars().skip(1).step_by(2).flat_map(|c| c.to_lowercase());
 
     first.interleave(second).collect()
 }
